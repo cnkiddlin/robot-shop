2eb24e3a2659613b105108a993a9eb19359871cf
cnkiddlin <kiddlin@hotmail.com>
2022-05-18 07:30:17 +0000

    test

--- jenkins-docker.sh
+++ jenkins-docker.sh
@@ -1,10 +1,13 @@
 export INSTANA_AGENT_KEY="qUMhYJxjSv6uZh2SyqTEnw"
 
+echo start build
 sudo docker compose build
-sudo nohup docker compose up >/dev/null 2>&1
+
+echo start up
+sudo nohup docker compose up
 
 current=`date "+%Y-%m-%d %H:%M:%S"`
 timeStamp=`date -d "$current" +%s`
 currentTimeStamp=$((timeStamp*1000+`date "+%N"`/1000000))
 
-curl -X POST https://159.23.91.46/api/releases -H "Authorization: apiToken 0howH44PRw2Aj2-3yg_BLQ" -H "Content-Type: application/json" -d '{"name": "Version 1.0", "start": 1652853972000, "applications": [{"name": "robot-shop-docker"}]}' -k
+curl -X POST https://159.23.91.46/api/releases -H "Authorization: apiToken 0howH44PRw2Aj2-3yg_BLQ" -H "Content-Type: application/json" -d '{"name": "New Release", "start": '${currentTimeStamp}', "applications": [{"name": "robot-shop-docker"}]}' -k
--- time.sh
+++
@@ -1,5 +0,0 @@
-current=`date "+%Y-%m-%d %H:%M:%S"`
-timeStamp=`date -d "$current" +%s`
-currentTimeStamp=$((timeStamp*1000+`date "+%N"`/1000000))
-
-curl -X POST https://159.23.91.46/api/releases -H "Authorization: apiToken 0howH44PRw2Aj2-3yg_BLQ" -H "Content-Type: application/json" -d '{"name": "Version 1.0", "start": 1652853972000, "applications": [{"name": "robot-shop-docker"}]}' -k
