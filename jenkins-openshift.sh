6261562035c82fdfe18b0306f888e759901d3ddc
cnkiddlin <kiddlin@hotmail.com>
2022-05-24 04:02:02 +0000

    fix bug

--- jenkins-openshift.sh
+++ jenkins-openshift.sh
@@ -18,6 +18,13 @@ cd K8s
 helm install robot-shop --set openshift=true --set image.repo=kiddlin -n robot-shop --set image.version=2.1.0 helm
 sleep 1m
 
-
 oc project robot-shop
 oc expose web
+
+line=`cat .env |head -n 3|tail -n 1`
+version=${line: 4}
+
+current=`date "+%Y-%m-%d %H:%M:%S"`
+timeStamp=`date -d "$current" +%s`
+currentTimeStamp=$((timeStamp*1000+`date "+%N"`/1000000))
+curl -X POST https://159.23.91.46/api/releases -H "Authorization: apiToken 0howH44PRw2Aj2-3yg_BLQ" -H "Content-Type: application/json" -d '{"name": "'${version}'", "start": '${currentTimeStamp}', "applications": [{"name": "robot-shop-openshift"}]}' -k
