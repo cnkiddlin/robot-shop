9c5296bc44840cbb77bc5f6817f65665a667a313
cnkiddlin <kiddlin@hotmail.com>
2022-06-01 13:50:39 +0000

    test

--- jenkins-openshift.sh
+++ jenkins-openshift.sh
@@ -29,4 +29,5 @@ timeStamp=`date -d "$current" +%s`
 Date=`date "+%N"`
 currentTimeStamp=$((timeStamp*1000+$((10#$Date))/1000000))
 
+
 curl -X POST https://159.23.91.46/api/releases -H "Authorization: apiToken 0howH44PRw2Aj2-3yg_BLQ" -H "Content-Type: application/json" -d '{"name": "'${version}'", "start": '${currentTimeStamp}', "applications": [{"name": "robot-shop-openshift"}]}' -k
